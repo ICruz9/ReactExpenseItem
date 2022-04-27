827ffad9bf6e64ee57c9152edcbdc0c9be8da373
icruz <ignacio1999montero@hotmail.com>
2022-04-27 11:57:54 -0600

    Adding new expense correction

--- src/components/ExpensesNew/JS/FormExpense.js
+++ src/components/ExpensesNew/JS/FormExpense.js
@@ -37,7 +37,7 @@ const FormExpense = (props) => {
             title: enteredTitle,
             amount: enteredAmount,
             date: new Date(enteredDate)
-        }
+        };
         props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
@@ -49,15 +49,15 @@ const FormExpense = (props) => {
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangedHandler}/>
+                <input type='text' value={enteredTitle} onChange={titleChangedHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangedHandler}/>
+                <input type='number' value={enteredAmount} min='0.01' step='0.01' onChange={amountChangedHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-01' max='2022-12-31' onChange={dateChangedHandler}/>
+                <input type='date' value={enteredDate} min='2019-01-01' max='2022-12-31' onChange={dateChangedHandler}/>
             </div>
         </div>
         <div className='new-expense__actions'>
