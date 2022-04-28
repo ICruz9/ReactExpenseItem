59d75f0b322872ef4c89e252fa2d43c65a6baea2
icruz <ignacio1999montero@hotmail.com>
2022-04-28 08:39:50 -0600

    Final Commit

--- src/components/Expenses/JS/Expenses.js
+++ src/components/Expenses/JS/Expenses.js
@@ -13,9 +13,6 @@ function Expenses(props) {
   const itemsFiltered = props.items.filter(item => {
     return enteredYear === item.date.getFullYear().toString(); 
   });
-  console.log(props.items);
-  
-  console.log(itemsFiltered);
   return (
     <div className="expenses">
       <ExpensesFilter selectedYear={enteredYear} onFilterByYear={filterByYear}/>
